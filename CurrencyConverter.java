62ce6961e58d68144b011c6f49b80cc291f82c27
kachai <servicios@kachai.io>
2025-10-20 15:28:06 +0100

    add CLP exchange functionallity

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -5,6 +5,10 @@ public class CurrencyConverter {
         double euroOverPoundRatio = scan.nextDouble();
         System.out.print("If I give you 1€, how many Sterling pounds will you give me? ");
         double poundOverEuroRatio = scan.nextDouble();
+        System.out.print("If I give you 1pound, how many CLP money will you give me? ");
+        double clpOverPoundRatio = scan.nextDouble();
+        System.out.print("If I give you 1CLP, how many Sterling pounds will you give me? ");
+        double poundOverClpRatio = scan.nextDouble();
         System.out.println("OK, now I know enough.");
         boolean finished = false;
         while (!finished) {
@@ -12,6 +16,8 @@ public class CurrencyConverter {
             System.out.println("What would you like to do?");
             System.out.println("   1 - Exchange pounds into euro");
             System.out.println("   2 - Exchange euro into pounds");
+            System.out.println("   3 - Exchange pounds into CLP");
+            System.out.println("   4 - Exchange CLP into pounds");
             System.out.println("   0 - Exit the program");
             System.out.println();
             System.out.print(  "> ");
@@ -28,6 +34,18 @@ public class CurrencyConverter {
                 euro   = scan.nextDouble();
                 pounds = euro * poundOverEuroRatio;
                 System.out.println(euro + "€ will give you £" + pounds);
+                break;
+            case 3:
+                System.out.print("How many pounds would you like to convert? ");
+                pounds = scan.nextDouble();
+                double clp = pounds * clpOverPoundRatio;
+                System.out.println("£" + pounds + " will give you " + clp + "CLP");
+                break;
+            case 4:
+                System.out.print("How many CLP would you like to convert? ");
+                clp = scan.nextDouble();
+                pounds = clp * poundOverClpRatio;
+                System.out.println(clp + "CLP will give you £" + pounds);
                 break;
             case 0:
                 finished = true;
